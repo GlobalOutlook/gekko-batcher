142621d0de7467434707c81bce95e70411dd181b
globaloutlook <GlobalOutlook247@gmail.com>
2019-11-25 21:55:56 +1000

    helper files

--- sample-config.js
+++ sample-config.js
@@ -85,18 +85,18 @@ config.method = 'neuralnet'
 //  }
 //}
 config.ranges = {
-  threshold_buy = 1.0,
-  threshold_sell = -1.0,
-  method = 'adadelta',
+  threshold_buy : 1.0,
+  threshold_sell : -1.0,
+  method : 'adadelta',
 
-  learning_rate = 1.2,
-  momentum = 0.9,
-  decay = 0.10,
-  stoploss_enabled = true|false,
-  stoploss_threshold = 0.90,
-  hodl_threshold = 1,
-  price_buffer_len = 100,
-  min_predictions = 1000
+  learning_rate : 1.2,
+  momentum : 0.9,
+  decay : 0.10,
+  stoploss_enabled : true|false,
+  stoploss_threshold : 0.90,
+  hodl_threshold : 1,
+  price_buffer_len : 100,
+  min_predictions : 1000
 }
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
